a75191ffd52bbb3f6e6a424f109cacc219b58d99
guog <guog@live.cn>
2024-12-06 18:36:02 +0800

    添加分析xls数据示例

--- llama-index/python/README.md
+++ llama-index/python/README.md
@@ -5,3 +5,18 @@
 - 安装python3
 - 安装 [Ollama](https://ollama.com/)
 - 获取模型 `ollama pull llama3.2`
+
+## 运行
+
+创建虚拟环境 	`python3 -m venv .venv/`
+
+激活虚拟环境 `source .venv/bin/active`
+
+安装依赖 `pip install -r requirements.txt`
+
+运行 `python starter.py`
+
+
+## 数据来源
+
+[国际统计局公开数据](https://data.stats.gov.cn/easyquery.htm?cn=E0105)
--- llama-index/python/starter.py
+++ llama-index/python/starter.py
@@ -2,7 +2,35 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 
-documents = SimpleDirectoryReader("data").load_data()
+# from llama_index.readers.file import (
+#   DocxReader,
+#   HWPReader,
+#   PDFReader,
+#   EpubReader,
+#   FlatReader,
+#   HTMLTagReader,
+#   ImageCaptionReader,
+#   ImageReader,
+#   ImageVisionLLMReader,
+#   IPYNBReader,
+#   MarkdownReader,
+#   MboxReader,
+#   PptxReader,
+#   PandasCSVReader,
+#   VideoAudioReader,
+#   UnstructuredReader,
+#   PyMuPDFReader,
+#   ImageTabularChartReader,
+#   XMLReader,
+#   PagedCSVReader,
+#   CSVReader,
+#   RTFReader,
+# )
+
+
+# parser = OfficeReader()
+# file_extractor = {".xls": parser}
+documents = SimpleDirectoryReader("./data").load_data()
 
 # bge-base embedding model
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
@@ -12,8 +40,9 @@ Settings.llm = Ollama(model="llama3.2", request_timeout=360.0)
 
 index = VectorStoreIndex.from_documents(
     documents,
+    show_progress=True
 )
 
 query_engine = index.as_query_engine()
-response = query_engine.query("GraphQL的优势有哪些?")
+response = query_engine.query("北京市2022年度商品房销售面积是多少?")
 print(response)
